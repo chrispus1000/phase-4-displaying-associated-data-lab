0c3dc94015dc7cd265cd7c06b741ff442b790e4a
chrispus1000 <minrevac@gmail.com>
2022-10-02 18:00:19 +0300

    commit tree 7b10d0686000b8ee853e1822d1bc1b0870cf7f5c

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+
+    def index
+        users = User.all
+        render json: users, include: :items, except: [:created_at,:updated_at]
+    end 
+    
+    def show 
+        user = User.find_by(id: params[:id])
+        render json: user, include: :items,  except: [:created_at,:updated_at]
+    end
 end
